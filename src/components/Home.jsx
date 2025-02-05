b9bf32ecb0928fe0e532c58f976c46edd56a3fd9
kaziabulhasib <hasibkazi420@gmail.com>
2025-02-05 11:25:44 +0530

    data fetching and mapping

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data);
+        console.log(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Books: {books.length}</h1>
+      <div className='book-container'>
+        {books.map((book, idx) => (
+          <h1 key={idx}>{book.book_name}</h1>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
