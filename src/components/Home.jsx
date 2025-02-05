0b06b78c196974929c0f7243b7c14a2d02273706
kaziabulhasib <hasibkazi420@gmail.com>
2025-02-05 16:58:48 +0530

    data loading using useLoaderData

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,21 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Book from "./Book";
-
+import { useLoaderData } from "react-router-dom";
 const Home = () => {
-  const [books, setBooks] = useState([]);
-  useEffect(() => {
-    fetch("data.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data);
-        console.log(data);
-      });
-  }, []);
+  // const [books, setBooks] = useState([]);
+  // useEffect(() => {
+  //   fetch("data.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setBooks(data);
+  //       console.log(data);
+  //     });
+  // }, []);
+  const books = useLoaderData();
   return (
     <div>
       <h1>Books: {books.length}</h1>
+
       <div className='book-container'>
         {books.map((book) => (
           <Book key={book.id} book={book} />
